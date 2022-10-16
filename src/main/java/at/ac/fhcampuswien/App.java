75741dbacef28e0d54d6282dbf8a138ddffa4987
tristan-cyber <tristanwestreicher@gmail.com>
2022-10-16 17:23:53 +0200

    Everything except Task 1 & 5!

--- src/main/java/at/ac/fhcampuswien/App.java
+++ src/main/java/at/ac/fhcampuswien/App.java
@@ -6,6 +6,12 @@ import java.util.Scanner;
 public class App {
 
     public static void oneMonthCalendar(int days, int start) {
+        int a;
+        int b;
+
+
+
+
         //modulo operator um nach 7 zeilenumbruch zu machen
         //mit if kann man zwei stellige zahlen machen damit vor einstelligen ein abstand ist
         //oder printf
@@ -82,6 +88,40 @@ public class App {
 
     }
 
+    public static String camelCase(String convertToCamelcase){
+        String test = "abcsdfwaer sdaf";
+        return test;
+    }
+
+    public static int checkDigit(int [] numbers){
+        int [] gewichtung = new int[numbers.length];
+        int [] produkt = new int[numbers.length];
+        int sum = 0;
+        int reminder = 0;
+        int checkDigit = 0;
+
+        for (int i = 0; i < numbers.length; i++){
+            gewichtung [i] = i +2;
+            produkt [i] = numbers [i] * gewichtung[i];
+            sum = sum + produkt [i];
+        }
+
+        reminder = sum % 11;
+
+        checkDigit = 11 - reminder;
+
+        if (checkDigit == 10){
+            checkDigit = 0;
+        } else if (checkDigit == 11) {
+            checkDigit = 5;
+        }
+
+        return checkDigit;
+
+
+
+    }
+
     // Implement all methods as public static
 
     public static void main(String[] args) {
@@ -96,8 +136,17 @@ public class App {
         long [] randomNumbers = lcg(0); //Methodenaufruf // was die Methode zrückliefert
         // steht dann in der Methode random Numbers
 
+
 */
 
+        /*
+        System.out.println("Task 3: Guessing Game");
+        System.out.println();
+        guessingGame(randomNumberBetweenOneAndHundred());
+
+
+        System.out.println();
+        System.out.println();
         System.out.println("Task 3: Swap Arrays");
 
         int [] array1 = {1,2,3,4};
@@ -112,6 +161,30 @@ public class App {
         System.out.println("After Swap:");
         System.out.println("Array1 => "+ java.util.Arrays.toString(array1));
         System.out.println("Array2 => "+ java.util.Arrays.toString(array2));
+
+
+        System.out.println("Task 5: camelcase");
+        System.out.println();
+        System.out.print("Convert into camelcase: ");
+        Scanner scr = new Scanner(System.in);
+        String convertToCamelCase = scr.nextLine();
+        CamelCase(convertToCamelCase);
+*/
+
+        System.out.println("Task 6: check digit");
+        System.out.println();
+        System.out.print("Array: ");
+        int [] numbers = {5, 4, 4, 1, 3, 8, 1,};
+        System.out.println(java.util.Arrays.toString(numbers));
+
+        System.out.print("Prüfziffer: ");
+        checkDigit(numbers);
+        System.out.println(checkDigit(numbers));
+
+
+
+
+
 
 
         // test your method implementations here
